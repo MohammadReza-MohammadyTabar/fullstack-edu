3cd5fb676a43150966fcacd4e56df384af772b07
Mohammad Reza Mohammady Tabar <52368257+Mohammadrezamohammady@users.noreply.github.com>
2023-09-22 18:49:08 +0330

    make server complete

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,11 +4,17 @@ import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthorModule } from './author/author.module';
+import {ConfigModule} from "@nestjs/config";
+import * as process from "process";
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/elearn'),CoursesModule,AuthorModule],
+  imports: [MongooseModule.forRoot(process.env.MONGO_DB),CoursesModule,AuthorModule,ConfigModule.forRoot({
+    isGlobal: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {Response,Request} from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use('*',(req:Request,res:Response)=>{
+    console.log(req.method  ,req.path)
+  })
   await app.listen(3200);
 }
 bootstrap();
