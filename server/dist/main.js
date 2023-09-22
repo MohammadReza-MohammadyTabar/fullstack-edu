c595f12b2de6297d2bfad808d02e389f82b20792
Mohammad Reza Mohammady Tabar <52368257+Mohammadrezamohammady@users.noreply.github.com>
2023-09-22 20:11:24 +0330

    bug fix

--- server/dist/main.js
+++ server/dist/main.js
@@ -4,8 +4,9 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.use('*', (req, res) => {
-        console.log(req.method, req.path);
+    app.use('*', (req, res, next) => {
+        console.log(req.method, req.originalUrl);
+        next();
     });
     await app.listen(3200);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {Response,Request} from "express";
+import {Response, Request, NextFunction} from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use('*',(req:Request,res:Response)=>{
-    console.log(req.method  ,req.path)
+  app.use('*',(req:Request,res:Response,next:NextFunction)=>{
+    console.log(req.method  ,req.originalUrl)
+    next()
   })
   await app.listen(3200);
 }
